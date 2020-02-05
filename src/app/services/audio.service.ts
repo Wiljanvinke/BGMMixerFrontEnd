2ebba46524669ba86c61ef5d2f94decc187a7cd8
Wiljan Vinke <wiljanvinke@gmail.com>
2020-02-05 13:23:14 +0100

    Added State Management

--- src/app/services/audio.service.ts
+++ src/app/services/audio.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Observable, BehaviorSubject, Subject } from "rxjs";
 import { takeUntil } from "rxjs/operators";
+import { StreamState } from '../interfaces/stream-state';
 import * as moment from "moment";
 
 @Injectable({
@@ -21,6 +22,26 @@ export class AudioService {
     "loadstart"
   ];
 
+  //Default StreamsState
+  private state: StreamState = {
+    playing: false,
+    readableCurrentTime: '',
+    readableDuration: '',
+    duration: undefined,
+    currentTime: undefined,
+    canplay: false,
+    error: false,
+  };
+
+  //TracksStateChanges
+  private stateChange: BehaviorSubject<StreamState> = new BehaviorSubject(
+    this.state
+  );
+
+  getState(): Observable<StreamState> {
+    return this.stateChange.asObservable();
+  }
+
   private streamObservable(url) {
     return new Observable(observer => {
       // Play audio
@@ -39,6 +60,8 @@ export class AudioService {
         this.audioObj.currentTime = 0;
         // remove event listeners
         this.removeEvents(this.audioObj, this.audioEvents, handler);
+        // reset state
+        this.resetState();
       };
     });
   }
@@ -79,4 +102,45 @@ export class AudioService {
     const momentTime = time * 1000;
     return moment.utc(momentTime).format(format);
   }
+
+  private updateStateEvents(event: Event): void {
+    switch (event.type) {
+      case "canplay":
+        this.state.duration = this.audioObj.duration;
+        this.state.readableDuration = this.formatTime(this.state.duration);
+        this.state.canplay = true;
+        break;
+      case "playing":
+        this.state.playing = true;
+        break;
+      case "pause":
+        this.state.playing = false;
+        break;
+      case "timeupdate":
+        this.state.currentTime = this.audioObj.currentTime;
+        this.state.readableCurrentTime = this.formatTime(
+          this.state.currentTime
+        );
+        break;
+      case "error":
+        this.resetState();
+        this.state.error = true;
+        break;
+    }
+    this.stateChange.next(this.state);
+  }
+
+  //Reset StreamState to default
+  private resetState() {
+    this.state = {
+      playing: false,
+      readableCurrentTime: '',
+      readableDuration: '',
+      duration: undefined,
+      currentTime: undefined,
+      canplay: false,
+      error: false
+    };
+  }
+
 }
